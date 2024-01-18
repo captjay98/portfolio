ed81a4ead8c9f9936609195c884b491fcdcdb853
captjay98 <captjay98@gmail.com>
2024-01-18 05:44:18 +0100

    changed my picture

--- components/home.jsx
+++ components/home.jsx
@@ -9,46 +9,47 @@ const Home = () => {
       bg-gray-900/10
          dark:bg-slate-900/30 dark:bg-gradient-to-br from-gray-700/50 via-transparent   "
     >
-      <div className="flex flex-col max-sm:mt-28 max-sm:mx-2 max-sm:w-full justify-center m-auto my-4  py-10 ">
-        <p className="md:text-[21px] lg:text-3xl lg:leading-tight max-sm:text-[21px] font-light tracking-wide leading-normal text-black dark:text-slate-400  text- ">
+      <div className="flex flex-col justify-center py-10 m-auto my-4 max-sm:mt-28 max-sm:mx-2 max-sm:w-full">
+        <p className="font-light tracking-wide leading-normal text-black lg:text-3xl lg:leading-tight max-sm:text-[21px] text- md:text-[21px] dark:text-slate-400">
           Hey there,<br></br> I am{" "}
           <span className="text-blue-600 font-italic max-sm:text-[26px] md:text-[28px] lg:text-[40px]">
             Jamal Ibrahim Umar,
           </span>{" "}
           <br></br>
-          <span class="leading-loose ">A FullStack Software Engineer.</span>
+          <span class="leading-loose">A FullStack Software Engineer.</span>
         </p>
-        <div className="pt-6 h-20 flex justify-center space-x-4">
+        <div className="flex justify-center pt-6 space-x-4 h-20">
           <Link href="https://www.github.com/captjay98">
             <Image
               src="/github.svg"
-              className=" dark:bg-blue-500/70 rounded-full shadow-2xl shadow-blue-500 w-10 h-auto"
+              className="w-10 h-auto rounded-full shadow-2xl shadow-blue-500 dark:bg-blue-500/70"
               width={35}
               height={35}
-              alt="Jamal's Picture"
+              alt="Github Logo"
             />
           </Link>
           <Link href="https://www.linkedin.com/in/captjay98">
             <Image
               src="/linkedin.svg"
-              className=" dark:bg-blue-500/70 rounded-full shadow-2xl shadow-blue-500 w-10 hover:w-12 h-auto"
+              className="w-10 h-auto rounded-full shadow-2xl hover:w-12 shadow-blue-500 dark:bg-blue-500/70"
               width={35}
               height={35}
               loading="eager"
-              alt="Jamal's Picture"
+              alt="Linkedin Logo"
             />
           </Link>
         </div>
       </div>
-      <div className="m-auto my-8  max-sm:my-1 max-sm:w-[350px] lg:w-[350px] ">
+      <div className="m-auto my-8 max-sm:my-1 max-sm:w-[350px] lg:w-[350px]">
         <Image
-          src="/jamal.webp"
-          className="rounded-md shadow-2xl w-full h-auto"
-          width={350}
-          height={350}
-          alt="A picture of Jamal wearing glasses"
+          src="/jamalbaby.webp"
+          className="m-auto w-auto h-auto rounded-md shadow-2xl"
+          width={300}
+          height={300}
+          alt="A picture of Jamal "
           loading="eager"
         />
+        {/* jamal.webp */}
       </div>
     </section>
   );
