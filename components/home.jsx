ef8bffa33876e77078e899111fcb78e042afe3d3
captjay98 <captjay98@gmail.com>
2024-07-06 01:15:18 +0100

    added flutter, bun and hono

--- components/home.jsx
+++ components/home.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-
+//TODO: Make pages multi pages
 const Home = () => {
   return (
     <section
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="flex flex-col justify-center py-10 m-auto my-4 max-sm:mt-28 max-sm:mx-2 max-sm:w-full">
         <p className=" font-light tracking-tight leading-normal text-black lg:text-xl lg:leading-tight max-sm:text-[1.25rem] text- md:text-[21px] dark:text-slate-400">
           <br></br>
-          <span class="leading-loose">$user-{'>'}fullstack_software_engineer;</span>
+          <span class="leading-loose">$user-{">"}fullstack_software_engineer;</span>
           <br></br>
           <span className="text-blue-600 font-italic max-sm:text-[1.75rem] md:text-[28px] lg:text-[40px]">
             Jamal Ibrahim Umar
@@ -42,13 +42,13 @@ const Home = () => {
           </Link>
         </div>
       </div>
-        <div className="m-auto rounded-full max-sm:my-1 max-sm:w-[350px] lg:w-[350px]">
-          <h1 class="text-blue-700 mb-2">Current Tech Stacks:</h1>
-      
-          <p class='text-slate-600 dark:text-slate-300 mb-1'> Laravel - Vue/Next - Postgres.</p>
-          <p class='text-slate-600  dark:text-slate-300'> Express - Vue/React/Next - Mongo/Postgres.</p>
-          
-        </div>
+      <div className="m-auto rounded-full max-sm:my-1 max-sm:w-[350px] lg:w-[350px]">
+        <h1 class="text-blue-700 mb-2">Current Tech Stacks:</h1>
+
+        <p class="text-slate-600 dark:text-slate-300 mb-[2px]"> Flutter</p>
+        <p class="text-slate-600 dark:text-slate-300 mb-[2px]"> Laravel - Vue/Next - Postgres</p>
+        <p class="text-slate-600  dark:text-slate-300"> Bun - Hono - React - Mongo/Postgres</p>
+      </div>
       {/* <div className="m-auto rounded-full max-sm:my-1 max-sm:w-[350px] lg:w-[350px]"> */}
       {/*   <Image */}
       {/*     src="/jamal.webp" */}
